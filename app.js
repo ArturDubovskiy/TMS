202a7a3e648aee46b6bbbe59fd3fd7f6644609d0
Artur Dubovskiy <sky.artur.sky@gmail.com>
2020-10-15 20:24:12 +0300

    Changes for heroku

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const path = require('path');
+// const cors = require('cors');
 
 const app = express();
 
@@ -11,7 +12,7 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.set('useCreateIndex', true);
 
 app.use(express.json({ extended: true}));
-app.use(cors());
+// app.use(cors());
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/tasks',  require('./routes/tasks.routes'));
 
